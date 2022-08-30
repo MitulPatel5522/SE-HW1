b64b0e5d03dd0fa28d19077430b1df33fe2ddc89
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-08-29 22:16:40 -0400

    added code coverage logic

--- .github/coverage.js
+++ .github/coverage.js
@@ -1,13 +1,33 @@
 const fs = require("fs");
 
+let json = JSON.parse(
+  fs.readFileSync("./coverage/coverage-summary.json").toString("utf-8")
+);
+
+const pct = json["total"]["functions"]["pct"];
+
+let color = "";
+
+if (pct > 80) {
+  color = "brightgreen";
+} else if (pct > 60) {
+  color = "green";
+} else if (pct > 50) {
+  color = "yellow";
+} else if (pct > 30) {
+  color = "orange";
+} else {
+  color = "red";
+}
+
 let str = fs.readFileSync("./README.md").toString("utf-8");
 str = str.replace(
   /<!-- Begin Code Coverage[^]*End Code Coverage -->/gm,
-  "<!-- Begin Code Coverage -->\n\
+  `<!-- Begin Code Coverage -->\n\
 \n\
-![Code Coverage](https://img.shields.io/badge/coverage-70%-yellow)\n\
+![Code Coverage](https://img.shields.io/badge/coverage-${pct}%-${color})\n\
 \n\
-<!-- End Code Coverage -->"
+<!-- End Code Coverage -->`
 );
 
 fs.writeFileSync("./README.md", str);
--- .github/coverage.sh
+++
@@ -1,9 +0,0 @@
-sed -i '/<!-- Begin Code Coverage -->/,/<!-- End Code Coverage -->/c\
-<!-- Begin Code Coverage -->\
-
-![Code Coverage](https://img.shields.io/badge/coverage-70%-brightgreen)\
-
-<!-- End Code Coverage -->' ./README.md
-
-
-
